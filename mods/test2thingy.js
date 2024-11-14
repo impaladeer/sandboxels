0c34877319058fb0bc0fb3ea862ed696c50f5afa
impaladeer <147952410+impaladeer@users.noreply.github.com>
2024-11-14 12:19:45 -0600

    Update test2thingy.js

--- mods/test2thingy.js
+++ mods/test2thingy.js
@@ -5,8 +5,6 @@ elements.coffeeeeeeee = {
     temp: 75,
     category: "TestTab",
     viscosity: 27,
-    tempHigh: 200,
-    stateHigh: ["steam", "fragrance"],
     state: "liquid",
     density: 308
 };
